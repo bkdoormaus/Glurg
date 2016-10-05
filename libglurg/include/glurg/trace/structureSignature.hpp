4ecfc2ae492c02dfebf7a7ae864e2b9aaa42afe4
[bk]door.maus <bkdoormaus@protonmail.com>
2016-10-05 09:40:45 -0400

    Began work on adding structure value types.
    - Added structure signature class.

---
+++ libglurg/include/glurg/trace/structureSignature.hpp
@@ -0,0 +1,46 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2016 [bk]door.maus
+
+#ifndef LIBGLURG_TRACE_STRUCTURE_SIGNATURE_HPP
+#define LIBGLURG_TRACE_STRUCTURE_SIGNATURE_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace glurg
+{
+	class FileStream;
+	class TraceFile;
+
+	class StructureSignature
+	{
+	public:
+		typedef std::uint32_t ID;
+
+		StructureSignature() = default;
+		~StructureSignature() = default;
+
+		ID get_id() const;
+
+		std::size_t get_num_fields() const;
+		std::string get_field_name(std::size_t index) const;
+
+		static StructureSignature* read(
+			ID id, TraceFile& trace, FileStream& stream);
+
+	private:
+		void set_num_fields(std::size_t value);
+		void set_field_name(std::size_t index, const std::string& value);
+
+		ID id;
+
+		std::vector<std::string> fields;
+	};
+}
+
+#endif
---
+++ libglurg/source/trace/structureSignature.cpp
@@ -0,0 +1,51 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright 2016 [bk]door.maus
+
+#include "glurg/trace/structureSignature.hpp"
+#include "glurg/trace/traceFile.hpp"
+
+glurg::StructureSignature::ID glurg::StructureSignature::get_id() const
+{
+	return this->id;
+}
+
+std::size_t glurg::StructureSignature::get_num_fields() const
+{
+	return fields.size();
+}
+
+std::string glurg::StructureSignature::get_field_name(std::size_t index) const
+{
+	return fields.at(index);
+}
+
+glurg::StructureSignature* glurg::StructureSignature::read(
+	ID id, TraceFile& trace, FileStream& stream)
+{
+	StructureSignature* signature = new StructureSignature();
+	signature->id = id;
+
+	std::size_t count = trace.read_unsigned_integer(stream);
+	signature->set_num_fields(count);
+
+	for (std::size_t i = 0; i < count; ++i)
+	{
+		signature->set_field_name(i, trace.read_string(stream));
+	}
+
+	return signature;
+}
+
+void glurg::StructureSignature::set_num_fields(std::size_t value)
+{
+	fields.resize(value);
+}
+
+void glurg::StructureSignature::set_field_name(
+	std::size_t index, const std::string& value)
+{
+	fields.at(index) = value;
+}
